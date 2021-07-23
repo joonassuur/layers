358bd95455a3bd16fb913cdfb2f6435750477f1a
Joonas <joonas.suurvali@gmail.com>
2021-07-23 15:29:44 +0300

    tileLayer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { useState, useRef, useEffect } from "react";
 import "ol/ol.css";
 import "./App.css";
 import { Map, View } from "ol";
-import Projection from 'ol/proj/Projection';
+import { get } from "ol/proj";
+import GeoJSON from 'ol/format/GeoJSON';
 import ImageWMS from "ol/source/ImageWMS";
-import { Image as ImageLayer } from "ol/layer";
+import { Stroke, Style } from "ol/style";
+import {
+  Image as ImageLayer,
+  Tile as TileLayer,
+  Vector as VectorLayer,
+} from "ol/layer";
+import TileGrid from "ol/tilegrid/TileGrid";
+import { bbox as bboxStrategy } from "ol/loadingstrategy";
+import VectorSource from "ol/source/Vector";
+import OSMXML from "ol/format/OSMXML";
+import XYZ from "ol/source/XYZ";
 
 function App() {
   const [map, setMap] = useState();
@@ -12,35 +23,66 @@ function App() {
   const mapElement = useRef();
   mapElement.current = map;
 
-  const projection = new Projection({
-    code: 'EPSG:3301',
-    units: 'm'
+  const projection = get("EPSG:3301");
+
+  const vectorSource = new VectorSource({
+    format: new GeoJSON({dataProjection: 'EPSG:3301'}),
+    projection,
+    url: function(extent) {
+      return (
+        "https://gsavalik.envir.ee/geoserver/maaamet/ows?" +
+        "service=WFS&version=1.0.0&request=GetFeature&" +
+        "typeName=ky_kehtiv&" +
+        "featurePrefix=maaamet&" +
+        "geometryPropertyName=geom&" +
+        "maxfeatures=100" +
+        extent.join(",") +
+        ",EPSG:3301"
+      );
+    },
   });
-  
+  const vector = new VectorLayer({
+    source: vectorSource,
+    style: new Style({
+      stroke: new Stroke({
+        color: "rgba(0, 0, 255, 1.0)",
+        width: 2,
+      }),
+    }),
+  });
+  const layer = new TileLayer({
+    source: new XYZ({
+      projection: 'EPSG:3301',
+      url: "https://tiles.maaamet.ee/tm/tms/1.0.0/foto/{z}/{x}/{-y}.jpg&ASUTUS=MAAAMET&KESKKOND=LIVE&IS=XGIS",
+      tileGrid: new TileGrid({
+        extent: [40500, 5993000, 1064500, 7017000],
+        minZoom: 3,
+        resolutions: [
+          4000, 2000, 1000, 500, 250, 125, 62.5, 31.25, 15.625, 7.8125,
+          3.90625, 1.953125, 0.9765625, 0.48828125,
+        ],
+      }),
+    }),
+
+  });
+
+
   useEffect(() => {
     if (!map) {
       const newMap = new Map({
         target: "map",
         layers: [
-          new ImageLayer({
-            source: new ImageWMS({
-              url: "http://kaart.maaamet.ee/wms/alus?",
-              params: {
-                LAYERS: "MA-ALUS",
-                VERSION: "1.1.1",
-              },
-            }),
-          }),
+          vector,
+          layer,
         ],
         view: new View({
           center: [550000, 6520000],
-          projection,
-          zoom: 10,
+          projection: 'EPSG:3301',
         }),
       });
       setMap(newMap);
     }
-  }, [map, setMap, projection]);
+  }, [map, setMap, projection, vector, layer]);
 
   return <div ref={mapElement} id="map"></div>;
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import proj4 from "proj4";
+import reportWebVitals from "./reportWebVitals";
+import { register } from "ol/proj/proj4";
+
+proj4.defs(
+  "EPSG:3301",
+  "+proj=lcc+lat_1=59.33333333333334+lat_2=58+lat_0=57.51755393055556+lon_0=24+x_0=500000+y_0=6375000+ellps=GRS80+towgs84=0,0,0,0,0,0,0+units=m+no_defs"
+);
+register(proj4);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
