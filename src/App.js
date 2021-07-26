cced2a989b82813203647cd71968f0ee4e2100f1
Joonas Suurväli <joonas.suurvali@concise.ee>
2021-07-26 15:41:33 +0300

    writeGetFeature

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ import {
   Tile as TileLayer,
   Vector as VectorLayer,
 } from "ol/layer";
+import {
+  and as andFilter,
+  equalTo as equalToFilter,
+  like as likeFilter,
+} from 'ol/format/filter';
 import TileGrid from "ol/tilegrid/TileGrid";
 import { bbox as bboxStrategy } from "ol/loadingstrategy";
 import VectorSource from "ol/source/Vector";
@@ -36,7 +41,6 @@ function App() {
         "geometryPropertyName=geom&" +
         "maxfeatures=100&" +
         "outputFormat=application/json&srsname=EPSG:3301&" +
-        'bbox=' +
         extent.join(",") +
         ",EPSG:3301"
       );
@@ -94,10 +98,40 @@ function App() {
               stopEvent: false,
             })
             const address = feature.values_.l_aadress
-            console.log('feat', newPopup)
+            // console.log('feat', feature)
             newPopup.setPosition(e.coordinate);
             map.addOverlay(newPopup);
             setPopup({ address })
+
+            const featureRequest = new WFS().writeGetFeature({
+              "baseUrl": "https://gsavalik.envir.ee/geoserver/maaamet/ows?",
+              "featureNS": "maaamet",
+              "type": "WFS",
+              "version": "1.0.0",
+              "featurePrefix": "maaamet",
+              "featureTypes": [
+                "ky_kehtiv"
+              ],
+              "outputFormat": 'application/json',
+              "geometryPropertyName": "geom",
+              filter: andFilter(
+                likeFilter('l_aadress', 'Soku tee 48a'),
+                equalToFilter('l_aadress', 'Soku tee 48a')
+              ),
+            });
+
+            fetch('https://gsavalik.envir.ee/geoserver/maaamet/ows', {
+              method: 'POST',
+              body: new XMLSerializer().serializeToString(featureRequest),
+            })
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (json) {
+                const features = new GeoJSON().readFeatures(json);
+                vectorSource.addFeatures(features);
+                map.getView().fit(vectorSource.getExtent());
+              });
           }
         })
       })
